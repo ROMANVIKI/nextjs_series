bf359bec3a82d06267b959a07432eb30f3616bbe
ROMANVIKI <vikiadhi2016@gmail.com>
2024-12-21 03:44:16 +0530

    changing btn

--- app/page.js
+++ app/page.js
@@ -58,7 +58,7 @@ const Home = () => {
             onClick={() => setIsImage(true)}
             className="mt-6 bg-blue-500 text-white px-4 py-2 rounded-lg shadow-md hover:bg-blue-600 transition duration-200"
           >
-           Show Project Strucutre Image
+           Show Project File Strucutre 
           </button>
         </div>
       )}
